bfbd94ea18dd2dcea57c021194ef1f9d7631e714
huq2090 <huq2090@gmail.com>
2025-08-21 16:33:18 -0500

    Option for single and batch image processing is added.

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ os.makedirs("outputs/figures", exist_ok=True)
 
 # Define path to input SEM image and known scale bar length in nanometers
 # image_path = "data/raw/SEM_nano_particles.png"
-image_path = "./SEM_Sample_Image.png"
+image_path = "./batch_images"  
 scale_bar_length_nm = 100  # known physical length
 
+# Select mode of analyzing images
+mode = "classical"   # choices later: "classical" | "ai" | "both" | "compare"
+batch = True        # True to process a folder
+
 # Instantiate analyzer and run the full pipeline
-analyzer = NanoparticleAnalyzer(image_path, scale_bar_length_nm)
+analyzer = NanoparticleAnalyzer(image_path, scale_bar_length_nm, batch=batch, mode = mode)
 analyzer.run()
